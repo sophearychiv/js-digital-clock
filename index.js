d2b2bdf3df3727ef9e080e2ae1f060c672fdd533
Sam Cheng (Tata Consultancy Services) <v-samnac@microsoft.com>
2019-05-27 15:32:27 -0700

    Completed digital clock

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code here
+function digitalClock() {
+    const date = new Date();
+    const hour = date.getHours();
+    const min = date.getMinutes();
+    const sec = date.getSeconds();
+
+    document.getElementById("clock").textContent = hour + " : " + min + " : " + sec;
+}
+
+setInterval(digitalClock, 1000);
